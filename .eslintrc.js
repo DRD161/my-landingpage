9ea64470c3850e449962d27d12fdfcad9b4db81b
DRD161 <dylan@dylandavenport.com>
2023-08-12 14:45:01 -0400

    Configure eslint and prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-    rules: {
-        semi: 'error',
-        'prefer-const': 'error',
-    },
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-            babelrc: false,
-            configFile: false,
-            // your babel options
-            presets: ['@babel/preset-react'],
-        },
-    },
-    extends: ['prettier'],
+  rules: {
+    semi: 'error',
+    'prefer-const': 'error',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
 };
