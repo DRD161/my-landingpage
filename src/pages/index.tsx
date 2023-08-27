53eb0051dc7d561342ffc8c78587fd5523a667b9
DRD161 <dylan@dylandavenport.com>
2023-08-27 15:03:40 -0400

    New types added for Cloudinary.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,25 @@ import Layout from '../components/Layout';
 import Header from '../components/Header';
 import SubHeader from '../components/SubHeader';
 import Social from '../components/Social';
+import IntroText from '../components/IntroText';
+
+// hooks
+import { useGetIcons } from '../hooks/use-get-icons';
 
 const IndexPage: React.FC<PageProps> = () => {
+  const { icons } = useGetIcons();
+
   return (
     <Layout>
       <>
         <Header firstName={'Dylan'} lastName={'Davenport'} />
         <SubHeader text="Front-End Developer and UX Enthusiast" />
-        <Social />
+        <Social icons={icons} />
+        <IntroText
+          text="I'm Dylan, a front-end developer with a passion for developing clean and intuitive user interface's with an
+          emphasis on accessibility and user friendliness. I specialize in using semantic markup along with JavaScript to
+          make dynamic data driven content."
+        />
       </>
     </Layout>
   );
